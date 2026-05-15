4e0044d8609dd162234db39117666710ffc5512b
unknown <nirupamsharma73@gmail.com>
2026-05-15 18:42:52 +0530

    fixed minor bug

--- hooks/useVolume.ts
+++ hooks/useVolume.ts
@@ -1,3 +1,5 @@
-export default function useVolume(){
-    const volume = useLayersStore((state: any) => state.volume);
+import useLayersStore from "@/store/useLayersStore";
+
+export default function useVolume() {
+  const volume = useLayersStore((state: any) => state.volume);
 }
